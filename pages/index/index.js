177eb3e78c4bf2a3be042b49ce01cfc4340dbf8d
shalouzaixiayu <884427640@qq.com>
2021-01-15 16:25:54 +0800

    第一天学习

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,24 +1,45 @@
 // pages/index/index.js
+const app = getApp()
+
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    msg: "你好， 我是msg",
+    student: {
+      name: "shy",
+      age: 19,
+      height: 187,
+    },
+    counter: 0
   },
 
+  // 绑定事件
+  increaseClick() {
+    this.setData({
+      counter: this.data.counter += 1
+    })
+  },
+  decreaseClick() {
+    this.setData({
+      counter: this.data.counter -= 1
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log("onload");
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    console.log("onready");
 
   },
 
@@ -26,6 +47,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    console.log("onshow");
 
   },
 
@@ -33,6 +55,7 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
+    console.log("onhide");
 
   },
 
@@ -46,10 +69,9 @@ Page({
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-
+  onPullDownRefresh(){
+    console.log("我下啦了");
   },
-
   /**
    * 页面上拉触底事件的处理函数
    */
